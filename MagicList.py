a85f3f8b9d9d2bcbde84914e156d6b97f7f8b36f
ofekraf <ofekraf+github@gmail.com>
2021-03-18 12:02:36 +0200

    MagicClass + basic tests

---
+++ MagicList.py
@@ -0,0 +1,17 @@
+
+
+class MagicList:
+
+
+    def __init__(self, cls_type=None):
+        self.clst_type = cls_type
+
+    def __getitem__(self, key):
+        # todo
+        pass
+
+    def __setitem__(self, key):
+        # todo
+        pass
+
+
---
+++ tests.py
@@ -0,0 +1,15 @@
+from MagicList import *
+
+
+def naive_test():
+    try:
+        a = MagicList()
+        assert a.string
+
+
+def TestMagicList():
+    naive_test()
+
+
+if __name__ == '__main__':
+    TestMagicList()
